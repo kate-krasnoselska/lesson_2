94bc4f1bc8cd8e7b8746e05480463935f0cde717
kate-krasnoselska <kate.krasnoselska@gmail.com>
2021-05-18 13:14:39 +0300

    first commit

---
+++ kate_krasnoselska/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace kate_krasnoselska
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
